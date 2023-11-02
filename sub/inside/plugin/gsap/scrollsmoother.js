d2f325adea69bd7954c97e6b9a6eac9a6662de66
Wj_ <142865132+wkd6262@users.noreply.github.com>
2023-11-02 12:01:56 +0900

    Add files via upload

--- sub/inside/plugin/gsap/scrollsmoother.js
+++ sub/inside/plugin/gsap/scrollsmoother.js
@@ -4,10 +4,12 @@ gsap.registerPlugin(ScrollTrigger);
 const container = document.querySelector(".smooth-scroll-container");
 const run = document.querySelector("img.run");
 const robot = document.querySelector("img.robot");
+const m_info1 = document.querySelector(".m_infotxt");
+const m_info2 = document.querySelector(".m_video h1");
 
 // ScrollTrigger를 사용하여 요소를 트리거하고 애니메이션을 정의
 gsap.to(".smooth-scroll-item", {
-  y: -2000, // 원하는 애니메이션 효과 설정
+  y: -500, // 원하는 애니메이션 효과 설정
   scrollTrigger: {
     trigger: container,
     start: "top top", // 트리거가 언제 시작될지 설정
@@ -21,15 +23,34 @@ gsap.to(".run", {
     trigger: run,
     start: "top center", // 트리거가 언제 시작될지 설정
     end: "bottom center", // 트리거가 언제 끝날지 설정
-    scrub: 5, // 스크롤 속도에 따라 애니메이션을 부드럽게 조절 (1은 100% 부드러움)
+    scrub: 7, // 스크롤 속도에 따라 애니메이션을 부드럽게 조절 (1은 100% 부드러움)
   },
 });
 gsap.to(".robot", {
-  x: 200, // 원하는 애니메이션 효과 설정
+  x: 300, // 원하는 애니메이션 효과 설정
   scrollTrigger: {
     trigger: robot,
-    start: "top center", // 트리거가 언제 시작될지 설정
+    start: "top bottom", // 트리거가 언제 시작될지 설정
+    end: "bottom bottom", // 트리거가 언제 끝날지 설정
+    scrub: 7, // 스크롤 속도에 따라 애니메이션을 부드럽게 조절 (1은 100% 부드러움)
+  },
+});
+gsap.to(".m_infotxt", {
+  y: -100, // 원하는 애니메이션 효과 설정
+  scrollTrigger: {
+    trigger: m_info1,
+    start: "top bottom", // 트리거가 언제 시작될지 설정
     end: "bottom center", // 트리거가 언제 끝날지 설정
-    scrub: 5, // 스크롤 속도에 따라 애니메이션을 부드럽게 조절 (1은 100% 부드러움)
+    scrub: 7, // 스크롤 속도에 따라 애니메이션을 부드럽게 조절 (1은 100% 부드러움)
+  },
+});
+
+gsap.to(".m_video h1", {
+  y: -80, // 원하는 애니메이션 효과 설정
+  scrollTrigger: {
+    trigger: m_info2,
+    start: "top bottom", // 트리거가 언제 시작될지 설정
+    end: "bottom center", // 트리거가 언제 끝날지 설정
+    scrub: 7, // 스크롤 속도에 따라 애니메이션을 부드럽게 조절 (1은 100% 부드러움)
   },
 });
