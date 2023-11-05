e984bf83e92fbd93b4a36f8059df2c3395658bcb
sol227 <142865364+sol227@users.noreply.github.com>
2023-11-05 12:39:09 +0900

    contact js 수정 및 추가 작업, sub common에 헤더, 푸터 css 추가

--- sub/contact/contact.js
+++ sub/contact/contact.js
@@ -1,28 +1,12 @@
-// map
-const mapContainer = document.getElementById("c_map"), // 지도를 표시할 div
-  mapOption = {
-    center: new kakao.maps.LatLng(37.57606916877362, 126.89055950772155), // 지도의 중심좌표
-    level: 3, // 지도의 확대 레벨
-    mapTypeId: kakao.maps.MapTypeId.ROADMAP, // 지도종류
-  };
-
-// 지도를 생성한다
-const map = new kakao.maps.Map(mapContainer, mapOption);
-
-// 지도에 마커를 생성하고 표시한다
-const marker = new kakao.maps.Marker({
-  position: new kakao.maps.LatLng(37.57606916877362, 126.89055950772155), // 마커의 좌표
-  map: map, // 마커를 표시할 지도 객체
-});
-
 // gsap.plugin
 gsap.registerPlugin(ScrollTrigger);
+
 // header menu
 const showNav = gsap
   .from(".h_pc_menu", {
     opacity: 0,
     paused: true,
-    duration: 0.2,
+    duration: 0.3,
   })
   .progress(1);
 
@@ -36,22 +20,12 @@ ScrollTrigger.create({
 });
 
 // scrollSmoder
-const container = document.querySelector(".contact");
 const mainText = document.querySelector(".c_txt");
 const content = document.querySelector(".smooth-scroll-container");
 const mapSmoder = document.querySelector(".c_map");
 
-gsap.to(".smooth-scroll-item", {
-  y: -500,
-  scrollTrigger: {
-    trigger: container,
-    start: "top top",
-    end: "bottom bottom",
-    scrub: 3,
-  },
-});
-gsap.to(".smooth-scroll-item", {
-  y: -200,
+gsap.to(".c_txt", {
+  y: -50,
   scrollTrigger: {
     trigger: mainText,
     start: "top top",
@@ -59,8 +33,8 @@ gsap.to(".smooth-scroll-item", {
     scrub: 2,
   },
 });
-gsap.to(".smooth-scroll-item", {
-  y: -80,
+gsap.to(".smooth-scroll-container", {
+  y: -40,
   scrollTrigger: {
     trigger: content,
     start: "top bottom",
@@ -68,12 +42,89 @@ gsap.to(".smooth-scroll-item", {
     scrub: 3,
   },
 });
-gsap.to(".smooth-scroll-item", {
-  y: -800,
+gsap.to(".c_map", {
+  y: -30,
   scrollTrigger: {
     trigger: mapSmoder,
     start: "top bottom",
     end: "bottom bottom",
-    scrub: 6,
+    scrub: 3,
   },
+});
+
+// QNA
+const innerQnaBtn = document.querySelector(".inner_qna_btn");
+const qnaSlideMenu = document.querySelector(".c_qna_menu");
+
+innerQnaBtn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  qnaSlideMenu.classList.toggle("active");
+});
+
+// QNA내에 checkbox
+const checkBoxes = document.querySelectorAll(".c_qna_check");
+
+checkBoxes.forEach((checkBox, idx) => {
+  checkBox.addEventListener("change", () => {
+    const label = document.querySelector(`[for="checkbox${idx}"]`);
+    if (checkBox.checked) {
+      label.style.color = "#ea5028";
+      label.style.borderColor = "#ea5028";
+    } else {
+      label.style.color = "black";
+      label.style.borderColor = "lightgray";
+    }
+  });
+});
+
+// brochure
+const brochurOpenBtn = document.querySelector(".inner_brochur_btn");
+const bsrochurCloseBtn = document.querySelector(".brochure_close_btn");
+const brochureModal = document.querySelector(".c_brochure_modal");
+// 모달 열기
+brochurOpenBtn.addEventListener("click", () => {
+  brochureModal.style.display = "block";
+});
+// 모달 닫기
+bsrochurCloseBtn.addEventListener("click", () => {
+  brochureModal.style.display = "none";
+});
+
+// map
+const mapContainer = document.getElementById("c_map"), // 지도를 표시할 div
+  mapOption = {
+    center: new kakao.maps.LatLng(37.57606916877362, 126.89055950772155), // 지도의 중심좌표
+    level: 3, // 지도의 확대 레벨
+    mapTypeId: kakao.maps.MapTypeId.ROADMAP, // 지도종류
+  };
+
+// 지도를 생성한다
+const map = new kakao.maps.Map(mapContainer, mapOption);
+
+// 지도에 확대 축소 컨트롤을 생성한다
+const zoomControl = new kakao.maps.ZoomControl();
+
+// 지도의 우측에 확대 축소 컨트롤을 추가한다
+map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+
+// 마커 이미지의 주소
+const markerImageUrl =
+    "https://jjc0822.github.io/Test_JJC/sub/contact/imgase/logo.svg",
+  markerImageSize = new kakao.maps.Size(40, 42), // 마커 이미지의 크기
+  markerImageOptions = {
+    offset: new kakao.maps.Point(20, 42), // 마커 좌표에 일치시킬 이미지 안의 좌표
+  };
+
+// 마커 이미지를 생성한다
+const markerImage = new kakao.maps.MarkerImage(
+  markerImageUrl,
+  markerImageSize,
+  markerImageOptions
+);
+
+// 지도에 마커를 생성하고 표시한다
+const marker = new kakao.maps.Marker({
+  position: new kakao.maps.LatLng(37.57606916877362, 126.89055950772155), // 마커의 좌표
+  image: markerImage, // 마커의 이미지
+  map: map, // 마커를 표시할 지도 객체
 });
---
+++ sub/contact/readme.md
@@ -0,0 +1,49 @@
+## sub_contact
+
+### 231028
+
+- html/css - 마크업 작업
+- 미디어쿼리 PC 작업 완성
+- 문의/신청 페이지 미완성
+
+### 231030
+
+- kakao-map이 pages에서 나오지 않는 오류 해결
+  => 이유: html 내에 <script scr="">에 https 누락
+- html - 문의/신청 페이지 마크업 작업
+
+### 231031
+
+- css - '프로젝트 문의하기' 부분 style 작업
+
+### 231101
+
+- css - '브로슈어 신청하기' 부분 style 작업
+- css - 문의/신청 페이지에 공통으로 들어가는 개인정보동의 checkbox 커스텀 작업
+- 문의/신청 페이지 미디어쿼리 PC 작업 1차
+
+### 231102
+
+- 미디어쿼리 PC 작업 2차
+- js - header 메인 메뉴에 gsapPlugin_ScrollTrigger 적용해서 스크롤을 움직일 때 사라졌다가 다시 나타나는 효과 줌
+- js - gsapPlugin_ScrollSmoder를 적용해서 스크롤을 움직일 때 각각의 콘텐츠들이 움직이는 효과를 주려고 했으나 실패
+
+### 231103
+
+- js - gsapPlugin_ScrollSmoder 적용 실패 해결
+  => 이유: 애니메이션을 적용시킬 선택자명이 틀려서
+- js 1차 - .addEventListener로 '프로젝트 문의하기'를 누르면 문의할 수 있는 toggle창을 구현했지만 닫았을 때 일부가 보이면서 실패
+  => 이유: overflow 속성를 이용해야하는 방법이었지만 그 부분에 margin이 아닌 padding을 사용했기 때문
+- css - '프로젝트 문의하기'부분의 toggle 완벽한 구현을 위해 기존 마크업 작업을 일부 수정
+  => form 전체에 주었던 padding을 각각의 부분에 margin으로 수정
+- js 2차 - css를 수정함으로써 toggle창 완성
+- js - .addEventListener로 '브로슈어 신청하기'를 누르면 모달창을 띄우려 했지만 모달창이 눌렀을 때 나오는 것이 아니라 페이지를 들어갔는 때부터 이미 떠있는 형식으로 구현
+
+### 231104
+
+- js - '브로슈어 신청하기'를 누르면 모달창 띄우기 실패 해결
+  => 이유: '복합 선택자'에 display: none;을 주고 .add와 .remove로 열고 닫으려니까 상황에 맞지 않는 형식으로 구현돼서 '복합 선택자'가 아닌 '기존 선택자'에 display: none;을 주고 js에서 .style.diplay로 여는 버튼에 block, 닫는 버튼에 none 속성을 주어서 해결
+- js - '프로젝트 문의하기' toggle창 내에 프로젝트 유형을 누르면 선택된 유형의 테두리와 글자 색상이 바뀌도록 함
+- css - 페이지 메인 제목 뒤에 gsapPlugin 중에 MotionPath를 적용 시킬 svg 이미지 넣고 위치와 크기를 조절하려는데 콘덴츠가 자꾸 사라졌지만 결국 해결
+  => 이유: 부모요소에 있던 overflow 때문 그래서 margin 대신 padding으로 여백 조절
+-
