455c6cca55207784fbeb9267f8d131ebe57b8a04
sol227 <142865364+sol227@users.noreply.github.com>
2023-11-07 00:29:39 +0900

    contact toggle창 js 추가

--- sub/contact/contact.js
+++ sub/contact/contact.js
@@ -21,7 +21,6 @@ ScrollTrigger.create({
 
 // scrollSmoder
 const mainText = document.querySelector(".c_txt");
-const content = document.querySelector(".smooth-scroll-container");
 const mapSmoder = document.querySelector(".c_map");
 
 gsap.to(".c_txt", {
@@ -33,15 +32,6 @@ gsap.to(".c_txt", {
     scrub: 2,
   },
 });
-gsap.to(".smooth-scroll-container", {
-  y: -40,
-  scrollTrigger: {
-    trigger: content,
-    start: "top bottom",
-    end: "bottom bottom",
-    scrub: 3,
-  },
-});
 gsap.to(".c_map", {
   y: -30,
   scrollTrigger: {
@@ -51,6 +41,20 @@ gsap.to(".c_map", {
     scrub: 3,
   },
 });
+// title motionPath
+gsap.registerPlugin(MotionPathPlugin);
+gsap.to("#motion_target", {
+  duration: 5,
+  repeat: "infinite",
+  motionPath: {
+    path: "#motion_path",
+    align: "#motion_path",
+    autoRotate: true,
+    alignOrigin: [0.5, 0.5],
+    target: "#motion_target",
+  },
+});
+
 // header side menu
 
 // QNA
@@ -63,11 +67,18 @@ innerQnaBtn.addEventListener("click", (evt) => {
 });
 
 // QNA내에 checkbox
-const checkBoxes = document.querySelectorAll(".c_qna_check");
+const checkBoxes = document.querySelectorAll(
+  ".c_qna_checkboxs > [type=checkbox]"
+);
+const projectInfo = document.querySelector(".c_qna_projact");
+const clientInfo = document.querySelector(".c_qna_client");
+const qnaBtns = document.querySelector(".c_qna_btns");
 
-checkBoxes.forEach((checkBox, idx) => {
+// 체크되면 체크박스 스타일 변경
+checkBoxes.forEach((checkBox) => {
+  const id = checkBox.id;
+  const label = document.querySelector(`[for="${id}"]`);
   checkBox.addEventListener("change", () => {
-    const label = document.querySelector(`[for="checkbox${idx}"]`);
     if (checkBox.checked) {
       label.style.color = "#ea5028";
       label.style.borderColor = "#ea5028";
@@ -76,6 +87,38 @@ checkBoxes.forEach((checkBox, idx) => {
       label.style.borderColor = "lightgray";
     }
   });
+});
+
+projectInfo.style.opacity = "0.5";
+projectInfo.style.pointerEvents = "none";
+clientInfo.style.opacity = "0.5";
+clientInfo.style.pointerEvents = "none";
+qnaBtns.style.opacity = "0.5";
+qnaBtns.style.pointerEvents = "none";
+// 체크박스를 선택한 후에 나타나는 효과
+checkBoxes.forEach((checkBox) => {
+  checkBox.addEventListener("change", () => {
+    if (checkBox.checked) {
+      projectInfo.style.opacity = "1";
+      projectInfo.style.pointerEvents = "auto";
+    } else {
+      projectInfo.style.opacity = "0.5";
+      projectInfo.style.pointerEvents = "none";
+    }
+  });
+});
+projectInfo.addEventListener("change", () => {
+  if (projectInfo.style.opacity === "1") {
+    clientInfo.style.opacity = "1";
+    clientInfo.style.pointerEvents = "auto";
+    qnaBtns.style.opacity = "1";
+    qnaBtns.style.pointerEvents = "auto";
+  } else {
+    clientInfo.style.opacity = "0.5";
+    clientInfo.style.pointerEvents = "none";
+    qnaBtns.style.opacity = "0.5";
+    qnaBtns.style.pointerEvents = "none";
+  }
 });
 
 // brochure
@@ -94,7 +137,7 @@ bsrochurCloseBtn.addEventListener("click", () => {
 // map
 const mapContainer = document.getElementById("c_map"), // 지도를 표시할 div
   mapOption = {
-    center: new kakao.maps.LatLng(37.57606916877362, 126.89055950772155), // 지도의 중심좌표
+    center: new kakao.maps.LatLng(37.57603768648452, 126.89061615602614), // 지도의 중심좌표
     level: 3, // 지도의 확대 레벨
     mapTypeId: kakao.maps.MapTypeId.ROADMAP, // 지도종류
   };
@@ -125,7 +168,7 @@ const markerImage = new kakao.maps.MarkerImage(
 
 // 지도에 마커를 생성하고 표시한다
 const marker = new kakao.maps.Marker({
-  position: new kakao.maps.LatLng(37.57606916877362, 126.89055950772155), // 마커의 좌표
+  position: new kakao.maps.LatLng(37.57603768648452, 126.89061615602614), // 마커의 좌표
   image: markerImage, // 마커의 이미지
   map: map, // 마커를 표시할 지도 객체
 });
