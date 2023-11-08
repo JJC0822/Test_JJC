992d91c1d0b8f6ea3aec4bf084c88c4499b88d85
soomin <142853911+choi030@users.noreply.github.com>
2023-11-08 15:16:28 +0900

    구름 넣기

--- main.js
+++ main.js
@@ -1,58 +1,62 @@
-
-const container = document.querySelector(".smooth-scroll-container");
+const gridContainer = document.querySelector(".grid_smooth-scroll-container");
+const gridSmoothScroll = document.querySelector(".grid_smooth_scroll");
 const group1 = document.querySelector(".group1");
 const group2 = document.querySelector(".group2");
 const group3 = document.querySelector(".group3");
 const group4 = document.querySelector(".group4");
 
-ScrollTrigger.matchMedia({
-        "(min-width: 800px)": function () {
-          gsap.to(".smooth-scroll-item", {
-            y: -100, 
-            scrollTrigger: {
-              trigger: container,
-              start: "top top", 
-              end: "bottom bottom", 
-              scrub: 2, 
-            },
-          });
-          gsap.to(".group1", {
-            y: 100, 
-            scrollTrigger: {
-              trigger: group1,
-              start: "top top", 
-              end: "bottom bottom",
-              scrub: 2, 
-            },
-          });
-          gsap.to(".group2", {
-            y: 100, 
-            scrollTrigger: {
-              trigger: group2,
-              start: "top top", 
-              end: "bottom bottom", 
-              scrub: 2, 
-            },
-          });
-          gsap.to(".group3", {
-            y: 100, 
-            scrollTrigger: {
-              trigger: group3,
-              start: "top top",
-              end: "bottom bottom", 
-              scrub: 2, 
-            },
-          });
-          gsap.to(".group4", {
-                y: 100,
-                scrollTrigger: {
-                  trigger: group4,
-                  start: "top top",
-                  end: "bottom bottom", 
-                  scrub: 2, 
-                },
-              });
-        };
-      
-        all: function () {},
-      });
+
+ScrollTrigger.create({
+  trigger:gridSmoothScroll,
+  start:"top top",
+  pin:true,
+  pinSpacing:false
+});
+ 
+    gsap.to(".grid_smooth-scroll-item", {
+      y: -100,
+      scrollTrigger: {
+        trigger: gridContainer,
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 2,
+      },
+    });
+    gsap.to(".group1", {
+      y: 100,
+      scrollTrigger: {
+        trigger: group1,
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 2,
+      },
+    });
+    gsap.to(".group2", {
+      y: 100,
+      scrollTrigger: {
+        trigger: group2,
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 2,
+      },
+    });
+    gsap.to(".group3", {
+      y: 100,
+      scrollTrigger: {
+        trigger: group3,
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 2,
+      },
+    });
+    gsap.to(".group4", {
+      y: 100,
+      scrollTrigger: {
+        trigger: group4,
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 2,
+      },
+    });
+ 
+
